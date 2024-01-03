f7267478df4e2aae47d10c249855c980a8fbe280
Allysa Foot <allysafoot@gmail.com>
2024-01-03 15:41:43 -0800

    my cat is harassing me please help

--- src/components/Convos.jsx
+++ src/components/Convos.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Crescent from '../img/crescent.jpg'
 
 const Convos = () => {
     return (
-        <div>Convos</div>
+        <div className='convos'>
+            <div className='userChat'>
+                <img src={Crescent} alt='' />
+                <div className='userChatInfo'>
+                    <span>Username</span>
+                    <p>What's up cuck</p>
+                </div>
+            </div>
+            <div className='userChat'>
+                <img src={Crescent} alt='' />
+                <div className='userChatInfo'>
+                    <span>Username</span>
+                    <p>gimme the fuckin nip</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navigation from './Navigation';
 import Search from './Search';
+import Convos from './Convos';
 
 function Sidebar() {
     return (
         <div className='sidebar'>
             <Navigation />
             <Search />
+            <Convos />
         </div>
     );
 }
