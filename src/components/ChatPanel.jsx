bfb93da6db1b9480d9f7f34e943ceded129d175b
Allysa Foot <allysafoot@gmail.com>
2024-01-03 16:56:48 -0800

    creating chat panel

--- src/components/ChatPanel.jsx
+++ src/components/ChatPanel.jsx
@@ -3,9 +3,15 @@ import React from 'react';
 const ChatPanel = () => {
     return (
         <div className='chatPanel'>
-            ChatPanel
+            <div className="chatInfo">
+                <span>Allysa</span>
+                <div className="chatIcons">
+                    <img src='' alt=' '/>
+                    <img src='' alt=' '/>
+                    <img src='' alt=' '/>
+                </div>
+            </div>
         </div>
     );
-};
-
+    };       
 export default ChatPanel;
